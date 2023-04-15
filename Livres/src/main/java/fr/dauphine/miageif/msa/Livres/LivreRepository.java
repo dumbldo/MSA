e3c9dff39eee275235fba15c69fe9d3bc9c9b824
Ludmila Hadj Arab <ludmila.hadjarab@se.linedata.com>
2023-04-15 17:08:12 +0200

    add LivreRepository

--- Livres/src/main/java/fr/dauphine/miageif/msa/Livres/LivreRepository.java
+++ Livres/src/main/java/fr/dauphine/miageif/msa/Livres/LivreRepository.java
@@ -3,4 +3,18 @@ package fr.dauphine.miageif.msa.Livres;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LivreRepository extends JpaRepository<Livre, Long> {
+    // lister
+    List<Livre> findAll();
+
+    // creer ou modifier
+    <S extends Livre> S save(S livre);
+
+    // recuperer un livre
+    Optional<Livre> findById(Long id);
+
+    // supprimer
+    void deleteById(long id);
+
+    // supprimer un liste de livres
+    void deleteAllByIdInBatch(Iterable<Long> ids);
 }
