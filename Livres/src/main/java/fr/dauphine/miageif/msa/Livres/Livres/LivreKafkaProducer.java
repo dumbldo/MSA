c1ab1fe45d9d96372136aff60fe17ae705037a4a
dumbldo <kerriche.sarah2017@gmail.com>
2023-04-16 15:12:34 +0100

    Add Kafka consumer & producer

---
+++ Livres/src/main/java/fr/dauphine/miageif/msa/Livres/Livres/LivreKafkaProducer.java
@@ -0,0 +1,16 @@
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LivreKafkaProducer {
+
+    @Autowired
+    private KafkaTemplate<String, Livre> kafkaTemplate;
+
+    public void sendLivre(String topic, Livre livre) {
+        kafkaTemplate.send(topic, livre);
+    }
+}
